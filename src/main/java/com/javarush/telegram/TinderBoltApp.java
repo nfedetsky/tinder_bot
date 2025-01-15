ddad4dcbb42ed257cf0c248b610d925bd04ac5f3
Nicholas Fedetsky <nfedetsky@gmail.com>
2025-01-16 02:02:12 +0300

    d_4.01.01 Задание четвертое

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -19,6 +19,9 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
     private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
     private DialogMode currentMode = null;
     private ArrayList<String> list = new ArrayList<>();
+    private UserInfo infoAboutMe;
+    private UserInfo infoAboutFriend;
+    private int questionCount;
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -104,6 +107,102 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
             }
             list.add(message);
             return;
+        }
+        //Mode PROFILE
+        if (message.equals("/profile")){
+            currentMode = DialogMode.PROFILE;
+            sendPhotoMessage("profile");
+            infoAboutMe = new UserInfo();
+            questionCount ++;
+            sendTextMessage("Для составление анкеты *ChatGPT* ответьте на несколько вопросов");
+            sendTextMessage("Вопрос: " + questionCount + " Сколько Вам лет?");
+            return;
+        }
+        if (currentMode == DialogMode.PROFILE){
+            switch (questionCount){
+                case 1:
+                    infoAboutMe.age = message;
+                    questionCount ++;
+                    sendTextMessage("Вопрос: " + questionCount + " Кем вы работаете?");
+                    return;
+                case 2:
+                    infoAboutMe.occupation = message;
+                    questionCount ++;
+                    sendTextMessage("Вопрос: " + questionCount + " Какое у Вас хобби?");
+                    return;
+                case 3:
+                    infoAboutMe.hobby = message;
+                    questionCount ++;
+                    sendTextMessage("Вопрос: " + questionCount + " Что Вам НЕ нравится в людях?");
+                    return;
+                case 4:
+                    infoAboutMe.annoys = message;
+                    questionCount ++;
+                    sendTextMessage("Вопрос: " + questionCount + " Какова цель знакомства?");
+                    return;
+                case 5:
+                    infoAboutMe.goals = message;
+                    questionCount = 0;
+                    String aboutMe = infoAboutMe.toString();
+                    String prompt = loadPrompt("profile");
+                    String answer = chatGPT.sendMessage(prompt,aboutMe);
+                    Message msg = sendTextMessage("Ожидайте, *ChatGPT* обрабатывает Ваш запрос");
+                    updateTextMessage(msg, answer);
+                    return;
+            }
+        }
+        //Mode OPENER
+        if (message.equals("/opener")){
+            currentMode = DialogMode.OPENER;
+            sendPhotoMessage("opener");
+            infoAboutFriend = new UserInfo();
+            questionCount ++;
+            sendTextMessage("Для составления анкеты *ChatGPT* пришли анкетные данные друга/подруги");
+            sendTextMessage("Впопрос" + questionCount + " Это кто, девушка или парень?");
+            return;
+        }
+        if (currentMode ==DialogMode.OPENER){
+            switch (questionCount) {
+                case 1:
+                    infoAboutFriend.sex = message;
+                    questionCount++;
+                    sendTextMessage("Вопрос: " + questionCount + " Сколько лет?");
+                    return;
+                case 2:
+                    infoAboutFriend.age = message;
+                    questionCount++;
+                    sendTextMessage("Вопрос: " + questionCount + " Как зовут?");
+                    return;
+                case 3:
+                    infoAboutFriend.name = message;
+                    questionCount++;
+                    sendTextMessage("Вопрос: " + questionCount + " Где живет?");
+                    return;
+                case 4:
+                    infoAboutFriend.city = message;
+                    questionCount++;
+                    sendTextMessage("Вопрос: " + questionCount + " Привлекателен/Привлекательна (по 10-бальной шкале?");
+                    return;
+                case 5:
+                    infoAboutFriend.handsome = message;
+                    questionCount++;
+                    sendTextMessage("Вопрос: " + questionCount + " Чем увлекается?");
+                    return;
+                case 6:
+                    infoAboutFriend.hobby = message;
+                    questionCount++;
+                    sendTextMessage("Вопрос: " + questionCount + " Какие цели для знакомства?");
+                    return;
+                case 7:
+                    infoAboutFriend.goals = message;
+                    questionCount = 0;
+                    String aboutFriend = infoAboutFriend.toString();
+                    String prompt = loadPrompt("profile");
+                    String answer = chatGPT.sendMessage(prompt,aboutFriend);
+                    Message msg = sendTextMessage("Ожидайте, *ChatGPT* обрабатывает Ваш запрос");
+                    updateTextMessage(msg, answer);
+                    return;
+            }
         }
     }
 
